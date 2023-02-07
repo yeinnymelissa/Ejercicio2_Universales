67eabf42381caf7d38dfadd86ef38f3c7088275b
yeinnymelissa <3017261750101@ingenieria.usac.edu.gt>
2023-02-06 22:38:37 -0600

    Secuencia en id pasteles

--- dataBases/src/main/java/com/ejercicio2/dataBases/entity/Pasteles.java
+++ dataBases/src/main/java/com/ejercicio2/dataBases/entity/Pasteles.java
@@ -4,7 +4,10 @@ import java.io.Serializable;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,6 +17,9 @@ public class Pasteles implements Serializable{
 	private static final long serialVersionUID = 7456281520975444160L;
 	
 	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO,
+		generator="sequenciaDePrueba") 
+	@SequenceGenerator(name="sequenciaDePrueba",sequenceName="sec_pasteles", allocationSize=1)
 	@Column(name="ID")
 	private int id;
 	
